61de27868807a572bca8dfb972afd4d2769a771e
sagun basnet <basnetsagun8@gmail.com>
2026-05-11 15:18:07 +0545

    js

---
+++ javascript/main.js
@@ -0,0 +1,47 @@
+let a;
+var b;
+const c = 10;
+
+// for ( i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+// if (a) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// const result = 1 + 2;
+// console.log(result);
+
+// const result2 = 4 + 5;
+// console.log(result2);
+// function myFun() {
+//   console.log("Hello");
+// }
+
+// function add(a, b) {
+//   //   console.log(a + b);
+//   console.log(b);
+
+// //   const res = a + b;
+// //   return res;
+// }
+// console.log(add(1, myFun()));
+
+// add(4, 5);
+
+// const add = (a, b) => a + b;
+// ()=> console.log("Hello");
+
+// add(4, () => {
+//   console.log("Hello");
+// });
+
+const arr = [1, 2, 3, 5, "hello", true, null, undefined, [1, 2, false]];
+
+// console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 3, 5, "hello", true, null, undefined, [1, 2, false]];
+
+// console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
