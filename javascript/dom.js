0579f394c4e0f939540d092581fb9f8c9d821512
sagun basnet <basnetsagun8@gmail.com>
2026-05-18 13:18:56 +0545

    timing function

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,31 +1,42 @@
-// const div = document.getElementById("myDiv");
-// const div = document.getElementsByClassName("container");
-// const div = document.getElementsByTagName("div");
+// // const div = document.getElementById("myDiv");
+// // const div = document.getElementsByClassName("container");
+// // const div = document.getElementsByTagName("div");
 
-const div = document.querySelector("div");
-const remove = document.querySelector("#remove");
-const add = document.querySelector("#add");
+// const div = document.querySelector("div");
+// const remove = document.querySelector("#remove");
+// const add = document.querySelector("#add");
 
-console.log(div);
+// console.log(div);
 
-const changeColor = () => {
-  div.style.backgroundColor = "black";
-};
-
-// const removeElement = () => {
-//   div.style.display = "none";
+// const changeColor = () => {
+//   div.style.backgroundColor = "black";
 // };
-// const addElement = () => {
+
+// // const removeElement = () => {
+// //   div.style.display = "none";
+// // };
+// // const addElement = () => {
+// //   div.style.display = "block";
+// // };
+
+// add.addEventListener("click", () => {
 //   div.style.display = "block";
-// };
+// });
 
-add.addEventListener("click", () => {
-  div.style.display = "block";
-});
+// remove.addEventListener("click", () => {
+//   div.style.display = "none";
+// });
 
-remove.addEventListener("click", () => {
-  div.style.display = "none";
-});
+// // div.innerHTML = "this is content form js";
+// const text = remove.innerHTML;
 
-// div.innerHTML = "this is content form js";
-const text = remove.innerHTML;
+// const btns = document.querySelectorAll("button");
+// console.log(btns);
+
+
+// for(let i = 0; i<btns.length; i++){
+//   btns[i].addEventListener("click", ()=>{
+//     document.body.style.backgroundColor = btns[i].innerHTML
+//     console.log("click");
+//   })
+// }
---
+++ javascript/timining.js
@@ -0,0 +1,26 @@
+// const timeOut = setTimeout(()=>{
+//     console.log(234);
+    
+// }, 2000)
+
+// if(login){
+
+//     clearTimeout(timeOut)
+// }
+
+
+
+let i= 0;
+let min = 0;
+
+const interval = setInterval(()=>{
+    // console.log(234);
+    i++;
+    console.log("Second: ", i, " Min: ", min);
+    if(i=== 15){
+        min++
+        i=0
+    }
+    
+    
+}, 1000)
