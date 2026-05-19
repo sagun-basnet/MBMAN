76651a3907d04893ac3b80adc980a110831490aa
sagun basnet <basnetsagun8@gmail.com>
2026-05-19 13:42:02 +0545

    es6

---
+++ javascript/module/fun.mjs
@@ -0,0 +1,14 @@
+export const add = (a,b) =>{
+    return a+b
+}
+export const sub = (a,b) =>{
+    return a-b
+}
+export const mul = (a,b) =>{
+    return a*b
+}
+export const div = (a,b) =>{
+    return a/b
+}
+
+// export {add, sub, mul, div}
---
+++ javascript/module/main.mjs
@@ -0,0 +1,3 @@
+import {add , sub, mul, div} from "./fun.mjs"
+
+console.log(add(5,5));
