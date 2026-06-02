a147845e457e3af4e4325da05717d94b48466ec2
sagun basnet <basnetsagun8@gmail.com>
2026-06-02 14:47:34 +0545

    form handling

--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -4,12 +4,34 @@ import Slider from "../components/home/Slider";
 import HeroSection from "../components/home/HeroSection";
 
 const HomePage = () => {
+  const api = [
+    {
+      id: 1,
+      name: "jhon",
+    },
+    {
+      id: 2,
+      name: "ram",
+    },
+    {
+      id: 3,
+      name: "bj",
+    },
+    {
+      id: 4,
+      name: "hari",
+    },
+  ];
   return (
-    <div>
-      <div className="flex">
-        <Slider />
-        <HeroSection />
-      </div>
+    <div className="grid grid-cols-4">
+      {api.map((item, index) => {
+        return (
+          <div className="w-[10rem] h-[10rem] border-2  flex flex-col justify-center items-center">
+            <span>{item.id}</span>
+            <h1>{item.name}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- frontend/src/page/LoginPage.jsx
+++ frontend/src/page/LoginPage.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  
 
-export default LoginPage
+  let allData = [];
+  const init = {
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(init);
+
+  const handleChange = (e) => {
+    // console.log(`${e.target.name}: ${e.target.value}`);
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    console.log(formData);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.email.length === 0) {
+      return alert("Email field required..");
+    }
+
+    if (formData.password.length < 7) {
+      return alert("Password must be of 8 char..");
+    }
+
+    console.log(formData);
+    allData.push(formData);
+
+    localStorage.setItem("user", JSON.stringify(allData));
+
+    setFormData(init);
+
+    const myUser = localStorage.getItem("user");
+    console.log(JSON.parse(myUser));
+
+    localStorage.removeItem("user");
+  };
+
+  // console.log(formData);
+  return (
+    <div className="h-screen grid place-items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="w-[30rem] p-8 shadow-2xl flex flex-col gap-4 rounded-lg "
+      >
+        <h1>Login </h1>
+        <input
+          onChange={handleChange}
+          className="p-2 w-full rounded-md border-2"
+          type="email"
+          placeholder="Enter email"
+          name="email"
+          value={formData.email}
+          required
+        />
+        <input
+          onChange={handleChange}
+          className="p-2 w-full rounded-md border-2"
+          type="password"
+          placeholder="Enter password"
+          name="password"
+          value={formData.password}
+          required
+        />
+        <input
+          className="p-2 px-4 rounded-md text-white font-bold bg-blue-600 cursor-pointer"
+          type="submit"
+          value="Register"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
